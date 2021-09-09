af519b93006756e840f65617b2d7f0db9ffcbd1e
ajeep <ajeep8@gmail.com>
2021-09-09 16:44:29 +0800

    modify embedMd()

--- R/local.R
+++ R/local.R
@@ -9,7 +9,7 @@
 embedMd <- function(subfile, addlevel=0){
 	res <- knitr::knit_child(subfile, quiet = TRUE)
 
-		if (addlevel != 0) {
+	if (addlevel != 0) {
 	  titlehead = strrep("#", addlevel+1)
 	  res=gsub("^#", titlehead, res)
 	}
